7d738a733b48b1ee05676bc12c7206a1d26989c4
zzorn <zzorn@iki.fi>
2013-11-16 17:39:56 +0200

    Added maven release scripts.

---
+++ release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Do maven release, then push the release created in the maven_repo project.
+mvn release:clean release:prepare release:perform && ../maven-repo/push_release.sh && mvn clean
+

1cf0a9102c0c61ea821cb383fef2ae2027292abd
zzorn <zzorn@iki.fi>
2013-11-16 18:17:26 +0200

    Push the next snapshot version after release is done.

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Do maven release, then push the release created in the maven_repo project.
-mvn release:clean release:prepare release:perform && ../maven-repo/push_release.sh && mvn clean
+mvn release:clean release:prepare release:perform && ../maven-repo/push_release.sh && mvn clean && git push
 
